a6b237a139953d52e18e9d538ef1bd5bbeb56bc4
Arnaud Morel <a.morel@hotmail.com>
2014-05-14 15:38:52 +0200

    Adding Dropzone.js system to upload file.

---
+++ eShopLab/Scripts/eShop.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    var myDropzone = $("#mydropzone").dropzone({
+        url: "/Uploads",
+        previewsContainer: ".dropzone-previews",
+        maxFiles: 1,
+        autoProcessQueue: false,
+        init: function () {
+            this.on("addedfile", function (file) {
+                var dz = Dropzone.forElement("#mydropzone");
+                if (dz.files.length > 1) {
+                    dz.removeFile(dz.files[0])
+                }
+            });
+        }
+    });
+
+    console.log("ready!");
+});
