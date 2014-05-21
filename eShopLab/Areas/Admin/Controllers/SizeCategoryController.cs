053af787539e677426f9ddaeabe61d786ebe3e75
Arnaud Morel <a.morel@hotmail.com>
2014-05-21 15:53:31 +0200

    Create SizeCategory & UnitType controller.

---
+++ eShopLab/Areas/Admin/Controllers/SizeCategoryController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eShopLab.Models;
+
+namespace eShopLab.Areas.Admin.Controllers
+{
+    public class SizeCategoryController : Controller
+    {
+        private eShopDBEntities db = new eShopDBEntities();
+
+        //
+        // GET: /Admin/SizeCategory/
+
+        public ActionResult Index()
+        {
+            return View(db.SizeCategories.ToList());
+        }
+
+        //
+        // GET: /Admin/SizeCategory/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            SizeCategory sizecategory = db.SizeCategories.Find(id);
+            if (sizecategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sizecategory);
+        }
+
+        //
+        // GET: /Admin/SizeCategory/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/SizeCategory/Create
+
+        [HttpPost]
+        public ActionResult Create(SizeCategory sizecategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SizeCategories.Add(sizecategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sizecategory);
+        }
+
+        //
+        // GET: /Admin/SizeCategory/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            SizeCategory sizecategory = db.SizeCategories.Find(id);
+            if (sizecategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sizecategory);
+        }
+
+        //
+        // POST: /Admin/SizeCategory/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(SizeCategory sizecategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sizecategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sizecategory);
+        }
+
+        //
+        // GET: /Admin/SizeCategory/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            SizeCategory sizecategory = db.SizeCategories.Find(id);
+            if (sizecategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sizecategory);
+        }
+
+        //
+        // POST: /Admin/SizeCategory/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SizeCategory sizecategory = db.SizeCategories.Find(id);
+            db.SizeCategories.Remove(sizecategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eShopLab/Areas/Admin/Controllers/UnitTypeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eShopLab.Models;
+
+namespace eShopLab.Areas.Admin.Controllers
+{
+    public class UnitTypeController : Controller
+    {
+        private eShopDBEntities db = new eShopDBEntities();
+
+        //
+        // GET: /Admin/UnitType/
+
+        public ActionResult Index()
+        {
+            return View(db.UnitTypes.ToList());
+        }
+
+        //
+        // GET: /Admin/UnitType/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UnitType unittype = db.UnitTypes.Find(id);
+            if (unittype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unittype);
+        }
+
+        //
+        // GET: /Admin/UnitType/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/UnitType/Create
+
+        [HttpPost]
+        public ActionResult Create(UnitType unittype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UnitTypes.Add(unittype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(unittype);
+        }
+
+        //
+        // GET: /Admin/UnitType/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UnitType unittype = db.UnitTypes.Find(id);
+            if (unittype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unittype);
+        }
+
+        //
+        // POST: /Admin/UnitType/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(UnitType unittype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unittype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unittype);
+        }
+
+        //
+        // GET: /Admin/UnitType/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UnitType unittype = db.UnitTypes.Find(id);
+            if (unittype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unittype);
+        }
+
+        //
+        // POST: /Admin/UnitType/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UnitType unittype = db.UnitTypes.Find(id);
+            db.UnitTypes.Remove(unittype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
