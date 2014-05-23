0f994343d672186f9cc0323a5f10acbfb215c9a8
Arnaud Morel <a.morel@hotmail.com>
2014-05-23 13:49:42 +0200

    Change menu.

--- eShopLab/App_Start/BundleConfig.cs
+++ eShopLab/App_Start/BundleConfig.cs
@@ -14,17 +14,27 @@ namespace eShopLab.App_Start
             bundles.Add(new ScriptBundle("~/bundles/css").Include(
                 "~/Content/style.css",
                 "~/Content/font-awesome.min.css",
+                "~/Content/bootstrap.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/cssadmin").Include(
+                "~/Content/font-awesome.min.css",
                 "~/Scripts/dropzone/css/basic.css",
                 "~/Scripts/dropzone/css/dropzone.css",
-                "~/Content/bootstrap.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/animate.css",
+                "~/Content/admin.css"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/script").Include(
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/dropzone/dropzone.js",
-                "~/Scripts/eShop.js",
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/modernizr-2.7.2.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/admin.js"
+                ));
         }
     }
 }
---
+++ eShopLab/Scripts/admin.js
@@ -0,0 +1,136 @@
+﻿jQuery.fn.exists = function () { return this.length > 0; }
+
+var app = function () {
+
+    var init = function () {
+
+        tooltips();
+        toggleMenuLeft();
+        toggleMenuRight();
+        menu();
+        togglePanel();
+        closePanel();
+        dropzone();
+    };
+
+    var tooltips = function () {
+        $('#toggle-left').tooltip();
+    };
+
+    var togglePanel = function () {
+        $('.actions > .fa-chevron-down').click(function () {
+            $(this).parent().parent().next().slideToggle('fast');
+            $(this).toggleClass('fa-chevron-down fa-chevron-up');
+        });
+
+    };
+
+    var toggleMenuLeft = function () {
+        $('#toggle-left').bind('click', function (e) {
+            if (!$('.sidebarRight').hasClass('.sidebar-toggle-right')) {
+                $('.sidebarRight').removeClass('sidebar-toggle-right');
+                $('.main-content-wrapper').removeClass('main-content-toggle-right');
+            }
+            $('.sidebar').toggleClass('sidebar-toggle');
+            $('.main-content-wrapper').toggleClass('main-content-toggle-left');
+            e.stopPropagation();
+        });
+    };
+
+    var toggleMenuRight = function () {
+        $('#toggle-right').bind('click', function (e) {
+
+            if (!$('.sidebar').hasClass('.sidebar-toggle')) {
+                $('.sidebar').addClass('sidebar-toggle');
+                $('.main-content-wrapper').addClass('main-content-toggle-left');
+            }
+
+            $('.sidebarRight').toggleClass('sidebar-toggle-right animated bounceInRight');
+            $('.main-content-wrapper').toggleClass('main-content-toggle-right');
+
+            if ($(window).width() < 660) {
+                $('.sidebar').removeClass('sidebar-toggle');
+                $('.main-content-wrapper').removeClass('main-content-toggle-left main-content-toggle-right');
+            };
+
+            e.stopPropagation();
+        });
+    };
+
+    var closePanel = function () {
+        $('.actions > .fa-times').click(function () {
+            $(this).parent().parent().parent().fadeOut();
+        });
+
+    }
+
+    var menu = function () {
+        $("#leftside-navigation .sub-menu > a").click(function (e) {
+            $("#leftside-navigation ul ul").slideUp();
+            if (!$(this).next().is(":visible")) {
+                $(this).next().slideDown();
+            }
+            e.stopPropagation();
+        });
+    };
+
+    var dropzone = function () {
+        var myDropzone = $("#mydropzone").dropzone({
+            previewsContainer: ".dropzone-previews",
+            maxFiles: 1,
+            addRemoveLinks: true,
+            autoProcessQueue: false,
+            acceptedFiles: "image/*",
+            init: function () {
+                this.on("addedfile", function (file) {
+                    var dz = Dropzone.forElement("#mydropzone");
+                    if (dz.files.length > 1) {
+                        dz.removeFile(dz.files[0])
+                    }
+                });
+
+                var myDropzone = this;
+
+                // First change the button to actually tell Dropzone to process the queue.
+                this.element.querySelector("input[type=submit]").addEventListener("click", function (e) {
+                    // Make sure that the form isn't actually being sent.
+                    if (Dropzone.forElement("#mydropzone").files.length > 0) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        if ($("#mydropzone").valid()) {
+                            myDropzone.processQueue();
+                        }
+                    }
+                });
+                if ($("input[name='fileUrl']").exists())
+                    if ($("input[name='fileUrl']").val() != "") {
+                        var fakeFile = {
+                            name: "",
+                            size: 2323 // in bytes,
+                        };
+
+                        myDropzone.emit("addedfile", fakeFile);
+                        myDropzone.emit("thumbnail", fakeFile, location.protocol + '//' + location.host + $("input[name='fileUrl']").val());
+                        myDropzone.files.push(fakeFile);
+                    }
+            },
+            complete: function () {
+                window.location = "./";
+            }
+        });
+
+    }
+
+    //End functions
+
+    //return functions
+    return {
+        init: init,
+    };
+}();
+
+//Load global functions
+$(document).ready(function () {
+    app.init();
+
+});
