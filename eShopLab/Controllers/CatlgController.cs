8d41386fc26006cd40c075d8c250ea346a6bc1fe
Arnaud MOREL <a.morel@hotmail.com>
2014-06-09 17:34:13 +0200

    Add Catalogue & Checkout Controller.

---
+++ eShopLab/Controllers/CatlgController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eShopLab.Controllers
+{
+    public class CatlgController : Controller
+    {
+        //
+        // GET: /Catlg/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
